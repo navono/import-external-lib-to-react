d855c064861436718209d017e6351895d8df39d6
navono <navono007@gmail.com>
2018-08-29 08:59:37 +0800

    dynamic load Checkbox

--- react-ts-host/src/App.tsx
+++ react-ts-host/src/App.tsx
@@ -14,8 +14,22 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <input type='button' value="load Checkbox" onClick={this.handleLoadCheckBox} />
+        <div id="checkBox"/>
       </div>
     );
+  }
+
+  private handleLoadCheckBox = (e: any) => {
+    import("../../lib/CheckBox")
+      .then((CheckBoxNS) => {
+          const checkBox = new CheckBoxNS.CheckBox({id: "checkBox", title: "Test title"});
+          checkBox.render();
+      })
+      .catch((err) => {
+        // tslint:disable-next-line no-console
+        console.log("Failed to load CheckBox", err);
+      });
   }
 }
 
