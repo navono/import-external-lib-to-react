1485131ba29a861557cd9c4dd6f787244c03d7e5
navono <navono007@gmail.com>
2018-12-04 16:02:08 +0800

    fix: 修改加载逻辑

--- react-ts-host/src/App.tsx
+++ react-ts-host/src/App.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import * as path from 'path';
 import './App.css';
 import logo from './logo.svg';
 import * as loadjs from 'loadjs';
 
 interface IState {
-  customComponent: any;
+  checkBox: any;
+  logControl: any;
 }
 
 class App extends React.Component<any, IState> {
@@ -13,13 +13,17 @@ class App extends React.Component<any, IState> {
     super(props);
 
     this.state = {
-      customComponent: null,
+      checkBox: null,
+      logControl: null,
     };
   }
 
   public render() {
-    const { customComponent } = this.state;
-    const enable = customComponent !== null;
+    const { checkBox, logControl } = this.state;
+    const checkBoxEnable = checkBox !== null;
+    const logControlEnable = logControl !== null;
+
+
     return (
       <div className="App">
         <header className="App-header">
@@ -30,8 +34,10 @@ class App extends React.Component<any, IState> {
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
         <input type='button' value="load Checkbox" onClick={this.handleLoadCheckBox} />
+        <input type='button' value="load LogControl" onClick={this.handleLogControl} />
         <div id="checkBox"/>
-        {enable && <this.state.customComponent/>}
+        {checkBoxEnable && <this.state.checkBox/>}
+        {logControlEnable && <this.state.logControl/>}
       </div>
     );
   }
@@ -41,34 +47,39 @@ class App extends React.Component<any, IState> {
     // 如果将整个模块路径保存到变量中传入 import，webpack会提示找不到模块
     // https://segmentfault.com/a/1190000015648036
 
-    const fileName = 'TestName';
-    const modName = 'AwesomeComponent'
-    // import('../../lib/' + modName)
-    
-    console.log(__dirname, process.cwd());
-    console.log(path.resolve(__dirname, '../../'));
-    // const basic = `../../`;
-    
-    console.log('prepare to load component');
-    
-    loadjs([`../../lib/${fileName}/index.js`], modName, {
-      success: () => {
-        this.setState({
-          customComponent: (window as any)[modName].default
-        });
-        console.log('load js ', (window as any)[modName].Config);
-      },
-    });
+    const fileName = 'CheckBox';
+    const libPath = `../../lib/${fileName}/index.js`;
+    const modName = 'AwesomeComponent';
 
-    // import(`${basic}lib/` + modName)
-    //   .then((CheckBoxNS) => {
-    //       const checkBox = new CheckBoxNS.CheckBox({id: "checkBox", title: "Test title"});
-    //       checkBox.render();
-    //   })
-    //   .catch((err) => {
-    //     // tslint:disable-next-line no-console
-    //     console.log("Failed to load CheckBox", err);
-    //   });
+    if (!loadjs.isDefined(modName)) {
+      loadjs([libPath], modName, {
+        success: () => {
+          this.setState({
+            checkBox: (window as any)[modName].default
+          });
+          console.log('load js ', (window as any)[modName].Config);
+        },
+      });
+    }
+  }
+
+  private handleLogControl = (e: any) => {
+    const fileName = 'LogControl';
+    const libPath = `../../lib/${fileName}/index.js`;
+    const modName = 'LogControl';
+
+    if (!loadjs.isDefined(modName)) {
+      loadjs([libPath], modName, {
+        success: () => {
+          console.log('load js ', (window as any)[modName].Config);
+          console.log((window as any)[modName].default);
+          
+          this.setState({
+            logControl: (window as any)[modName].default
+          });
+        },
+      });
+    }
   }
 }
 
